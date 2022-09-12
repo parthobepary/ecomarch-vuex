b654278f640b41ad92dce07bac2deb4b241cfa11
pallobbiswas <pallobbiswas326@gmail.com>
2022-09-12 15:01:51 +0600

    data shorted added

---
+++ src/components/AllProducts.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="itemcard">
+      <div class="items" v-for="item in products" v-bind:key="item.id">
+        <h2>Name: {{ item.title }}</h2>
+        <img class="image" :src="item.image" alt="" />
+        <p>Price: {{ item.price }}</p>
+        <p>Rating: {{ item.rating.rate }}</p>
+        <p>Count: {{ item.rating.count }}</p>
+        <button @click="addToCars(item)" class="">Add to card</button>
+        <!-- <ProductCard :product="item" /> -->
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "AllProducts",
+  props: {
+    products: Array,
+  },
+};
+</script>
+<style lang="scss">
+.itemcard {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  .items {
+    background-color: rgb(2, 116, 8);
+    margin: 10px;
+    padding: 5px 0;
+    text-align: center;
+    border-radius: 10px;
+  }
+  img {
+    width: 100px;
+    max-width: 100%;
+  }
+}
+</style>
--- src/components/ProductsSec.vue
+++ src/components/ProductsSec.vue
@@ -1,10 +1,71 @@
 <template>
-  <div class="banner">
-    <h1>This is products</h1>
+  <div class="products">
+    <h1 class="title">Choice your best products</h1>
+    <div class="body-container">
+      <div class="shorting">
+        <div class="stiky">
+          <div class="shortestList">
+            <button @click="shortPrice(50)">Lowest price</button>
+            <br />
+            <button @click="shortPrice(100)">Heightest price</button>
+            <br />
+            <button @click="shorrate(3.8)">Best rating</button>
+            <br />
+            <button @click="shorrate(3)">lowest rating</button>
+            <br />
+            <button @click="showAll">Show all</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <AllProductsVue :products="shortedData"></AllProductsVue>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import AllProductsVue from "./AllProducts.vue";
 export default {
   name: "ProductsSec",
+  components: {
+    AllProductsVue,
+  },
+  data() {
+    return {
+      produtc: [],
+      shortedData: [],
+    };
+  },
+  methods: {
+    shortPrice(value) {
+      const valus = this.produtc;
+      const result = valus.filter((element) => element?.price <= value);
+      this.shortedData = [...result];
+      console.log(this.shortedData);
+    },
+    shorrate(rating) {
+      const valus = this.produtc;
+      const result = valus.filter((element) => element?.rating?.rate > rating);
+      this.shortedData = [...result];
+      console.log(this.shortedData);
+    },
+    showAll() {
+      this.shortedData = [...this.produtc];
+    },
+    getData() {
+      this.axios.get("https://fakestoreapi.com/products").then((response) => {
+        this.produtc = response.data;
+        this.shortedData = [...this.produtc];
+      });
+    },
+  },
+  mounted() {
+    this.getData();
+  },
 };
 </script>
+<style lang="scss">
+.products {
+  padding: 10px 20px;
+}
+</style>
