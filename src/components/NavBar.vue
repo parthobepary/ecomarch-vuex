7c985ed7cbff9a1a1c96d225832220180db0d625
pallobbiswas <pallobbiswas326@gmail.com>
2022-09-12 11:46:26 +0600

    start

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar">
+    <div>
+      <h1>Phone <span>Buzz</span></h1>
+    </div>
+    <nav class="navitem">
+      <router-link class="item" to="/">Home</router-link>
+      <router-link class="item" to="/about">About</router-link>
+      <router-link class="item" to="/about">Card</router-link>
+      <router-link class="item" to="/about">Contact</router-link>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "./assets/scss/main.scss";
 import router from "./router";
 import store from "./store";
 
