75645465f86eb2e0c38f76b561ff11c44ab0dac8
pallobbiswas <pallobbiswas326@gmail.com>
2022-09-13 12:42:03 +0600

    invoice added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
-    <h1>Your total order : {{ $store.state.carts.length }}</h1>
+    <!-- <h1>Your total order : {{ dataArray.length }}</h1> -->
+    <h1>
+      {{
+        dataArray.length
+          ? `Your Total order is : ${dataArray.length}`
+          : "There is no product ☢"
+      }}
+    </h1>
     <div>
       <div class="items" v-for="item in dataArray" v-bind:key="item.index">
         <h2 class="bg-color">
@@ -17,6 +24,29 @@
         <!-- <ProductCard :product="item" /> -->
       </div>
     </div>
+    <div>
+      <div>
+        <div>
+          <div class="items" v-for="item in dataArray" v-bind:key="item.index">
+            <p class="">
+              Name: {{ item.title }}
+              <span class="spacebetwwen">Price: {{ item.price }}</span>
+              <span>Quantity: {{ item.quantity }}</span>
+            </p>
+            <!-- <ProductCard :product="item" /> -->
+          </div>
+        </div>
+        <div>
+          <hr />
+          <p>Sub total : {{ subTotalPrice }}</p>
+          <p>Vat: 20%</p>
+          <hr />
+          <p>Total: {{ total }}</p>
+        </div>
+        <!-- <ProductCard :product="item" /> -->
+        <button>Dowonload</button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -25,12 +55,26 @@ export default {
   data() {
     return {
       dataArray: this.$store.state.carts,
+      perPriceName: "",
+      perPrice: 0,
+      subTotalPrice: 0,
+      total: 0,
     };
   },
   methods: {
     xbutton(id) {
       this.$store.state.carts.splice(id, 1);
     },
+    invoice() {
+      this.dataArray.map((d) => {
+        this.perPrice = d.price * d.quantity;
+        this.subTotalPrice = this.subTotalPrice + this.perPrice;
+        this.total = (this.subTotalPrice + this.subTotalPrice * 0.2).toFixed(2);
+      });
+    },
+  },
+  mounted() {
+    this.invoice();
   },
 };
 </script>
